4fe20d246b39031995e84285db75423a4a4a8ad2
Ali Nooner <alinooner@gmail.com>
2022-03-04 16:06:44 -0600

    Add fetch calls for getAllData to index.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@
 // <<<>>> QUERY SELECTORS <<<>>>
 
 // <<<>>> EVENT HANDLERS <<<>>>
+
+const getAllData = (userID)  => {
+  let getTravelerData = fetch('http://localhost:3001/api/v1/travelers')
+  .then(response => response.json())
+  let getTripData = fetch('http://localhost:3001/api/v1/trips')
+  .then(response => response.json())
+  let getDestinationData = fetch('http://localhost:3001/api/v1/destinations')
+  .then(response => response.json())
+  let getSingleTravelerDara = fetch('http://localhost:3001/api/v1/travelers/${userID}')
+  .then(response => response.json())
+}
