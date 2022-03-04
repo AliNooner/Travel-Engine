a657518efda1c9bcddb4efaa1b6496285ff3d411
Ali Nooner <alinooner@gmail.com>
2022-03-03 19:43:37 -0600

    Add css file

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,43 @@
+// const path = require('path');
+//
+// module.exports = {
+//   "mode": "none",
+//   "entry": "./src/scripts.js",
+//   "output": {
+//     "path": __dirname + '/dist',
+//     "filename": "bundle.js",
+//     sourceMapFilename: "bundle.js.map"
+//   },
+//   devServer: {
+//     contentBase: path.join(__dirname, 'dist')
+//   },
+//   "devtool": "source-map",
+//   "module": {
+//     "rules": [
+//       {
+//         test: /\.css$/,
+//         use: [
+//             { loader: 'style-loader' },
+//             { loader: 'css-loader' }
+//         ]
+//     },
+//       {
+//         test: /\.(png|svg|jpg|gif)$/,
+//         use: [
+//           {
+//             loader: 'file-loader',
+//             options: {
+//               name: '[name].[ext]',
+//               outputPath: 'images/',
+//               publicPath: 'images/'
+//             }
+//           }
+//         ]
+//       }
+//     ]
+//   }
+// };
+
 const path = require('path');
 
 module.exports = {
@@ -36,4 +76,4 @@ module.exports = {
       }
     ]
   }
-};
+}
