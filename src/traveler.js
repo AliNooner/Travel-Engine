0b575383fbb53f6ec482cafc6164b15708d8a0f2
Ali Nooner <alinooner@gmail.com>
2022-03-05 14:09:51 -0600

    Add ability to find all trips for traveler in traveler.js

--- src/traveler.js
+++ src/traveler.js
@@ -5,10 +5,36 @@ class Traveler {
     this.travelerID = travelerData.id;
     this.travelerName = travelerData.name;
     this.travelerType = travelerData.travelerType;
-    this.trips = [];
-    this.tripsThisYear = [];
-    this.totalSpentThisYear = [];
+    // this.trips = [];
+    // this.tripsThisYear = [];
+    // this.totalSpentThisYear = [];
+    this.allTrips = [];
+    this.pastTrips = [];
+    this.currentTrips = [];
+    this.upcomingTrips = [];
+    this.pendingTrips = [];
   }
+
+  // calculateAnnualCost() {
+  //   let totalAnnualCost = this.tripsThisYear.reduce((acc, currTrip) => {
+  //     acc += currTrip.
+  //   })
+  // }
+
+  findAllTrips(tripData, destinationData) {
+    let filteredTrips = tripData.filter(trip => trip.userID === this.travelerID);
+    let currentDestination;
+    filteredTrips.forEach(trip => {
+      destinationData.forEach(destination => {
+        if(trip.destinationID === destination.id) {
+          trip.currentDestination = destination;
+          this.allTrips.push(trip)
+        }
+      })
+    })
+    return this.allTrips;
+  }
+
 }
 
 
