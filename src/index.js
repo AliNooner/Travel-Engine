de4ca9145040031b8a693c7eec49ab70f67b8cd0
Ali Nooner <alinooner@gmail.com>
2022-03-04 18:28:43 -0600

    Add promise.all to index.js

--- src/index.js
+++ src/index.js
@@ -13,6 +13,12 @@ const getAllData = (userID)  => {
   .then(response => response.json())
   let getDestinationData = fetch('http://localhost:3001/api/v1/destinations')
   .then(response => response.json())
-  let getSingleTravelerDara = fetch('http://localhost:3001/api/v1/travelers/${userID}')
+  let getSingleTravelerData = fetch('http://localhost:3001/api/v1/travelers/${userID}')
   .then(response => response.json())
+
+  Promise.all([getTravelerData, getTripData, getDestinationData, getSingleTravelerDara])
+  .then(data => createDatasets(data[0], data[1], data[2], data[3]))
+  .catch(error => window.alert("Oops! Something went wrong!"))
 }
+
+// create a function that assigns data sets to variables for each data set
