63b8323bb5f613e2fd45d010896af257761a44c3
Ali Nooner <alinooner@gmail.com>
2022-03-06 13:50:23 -0600

    Add ability to display yearly trip cost

--- src/DOMUpdates.js
+++ src/DOMUpdates.js
@@ -1,10 +1,31 @@
+export const domUpdates = {
 
+  greetUser(traveler) {
+    const greetingMessage = document.getElementById('.greeting');
+    const firstName = traveler.name.split(''[0]);
+    greetingMessage.innerText = `Welcome, ${firstName}!`;
+  }
 
+  displayYearlyTripCost(traveler) {
+    const annualTripCost = document.getElementById('annual-spending');
+    const yearlyCost = traveler.calculateAmountSpentThisYear();
 
+    if(yearlyCost !== 0) {
+      annualTripCost.innerText = `You have spent $${yearlyCost} on trips so far this year`
+    } else {
+        annualTripCost.innerText = `You have not taken any trips this year`
+    }
+  }
 
+  }
 
-
-
-
-
-module exports domUpdates;
+  // createDropMenu(destinationData) {
+  //   const dropMenu = document.getElementById("trip-destination");
+  //
+  //   let allDestinationsList = "";
+  //   destinationData.forEach((destination) => {
+  //     allDestinationsList += `<option> value= "${destination.id}">${destination.destination}</option>`;
+  //   });
+  //   dropMenu.insertAdjacentHTML("beforeend", allDestinationsList);
+  // },
+};
