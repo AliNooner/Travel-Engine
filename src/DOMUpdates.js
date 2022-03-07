90f4b3189e71c7e706114c00b4db0773263ddc54
Ali Nooner <alinooner@gmail.com>
2022-03-06 18:10:44 -0600

    Add ability to greet user and display trip cards

--- src/DOMUpdates.js
+++ src/DOMUpdates.js
@@ -4,7 +4,7 @@ export const domUpdates = {
     const greetingMessage = document.getElementById('.greeting');
     const firstName = traveler.name.split(''[0]);
     greetingMessage.innerText = `Welcome, ${firstName}!`;
-  }
+  },
 
   displayYearlyTripCost(traveler) {
     const annualTripCost = document.getElementById('annual-spending');
@@ -15,17 +15,47 @@ export const domUpdates = {
     } else {
         annualTripCost.innerText = `You have not taken any trips this year`
     }
-  }
+  },
 
-  }
+  displayTripCards(traveler, allDestinations, cardType) {
+    let cardSection = document.getElementById('card-section')
+    cardSection.innerHtml = '';
 
-  // createDropMenu(destinationData) {
-  //   const dropMenu = document.getElementById("trip-destination");
-  //
-  //   let allDestinationsList = "";
-  //   destinationData.forEach((destination) => {
-  //     allDestinationsList += `<option> value= "${destination.id}">${destination.destination}</option>`;
-  //   });
-  //   dropMenu.insertAdjacentHTML("beforeend", allDestinationsList);
-  // },
+    if(traveler[cardType].length > 0) {
+      traveler[cardType].forEach(trip => {
+        const date = new Date(trip.date)
+        const splitDate = date.toLocaleString('en-US').split(',');
+        const formattedDate = splitDate[0]
+
+        let cardInfo =
+        `<section class="card">
+          <div class= "image-section">
+            <img class= "trip-image" src= "${trip.currentDestination.imgae}" alt = "${trip.currentDestination.alt}">
+          </div>
+          <div class= "info-section">
+            <p>${trip.currentDestination.destination.toUpperCase()}</p>
+            <p>Trip Date: ${formattedDate}</p>
+            <p>Travelers: ${trip.travelers}</p>
+            <p>Number of Days: ${trip.duration}</p>
+            <p>Status: ${trip.status}</p>
+          </div>
+        </section>`
+          cardSection.insertAdjacentHTML('beforeend', cardInfo)
+      })
+    } else {
+      cardSection.innerHTML = `<p class= "no-trip">There are no trips to display</p>`
+    }
+  },
+
+// }
+
+  createDropMenu(destinationData) {
+    const dropMenu = document.getElementById('trip-destination');
+
+    let allDestinationsList = "";
+    destinationData.forEach((destination) => {
+      allDestinationsList += `<option> value= "${destination.id}">${destination.destination}</option>`;
+    });
+    dropMenu.insertAdjacentHTML("beforeend", allDestinationsList);
+  }
 };
--- src/scripts.js
+++ src/scripts.js
@@ -8,15 +8,15 @@
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
 
-console.log("This is the JavaScript entry file - your code begins here.");
+// console.log("This is the JavaScript entry file - your code begins here.");
 
 // <<<>>> IMPORTS <<<>>>
 import "./css/styles.css";
-import fetchAPIdata from "./apiCalls.js";
+import { fetchAPIdata } from "./apiCalls.js";
+import { domUpdates } from './domUpdates.js'
 import Traveler from "./traveler.js";
-import Destination from "./destination.js"
+// import Destination from "./destination.js"
 import Trip from "./trip.js"
-import domUpdates from '.domUpdates'
 
 // <<<>>> GLOBAL VARIABLES <<<>>>
 let traveler;
@@ -25,9 +25,7 @@ let travelerIndex;
 
 
 // <<<>>> QUERY SELECTORS <<<>>>
-
-// <<<>>> EVENT HANDLERS <<<>>>
-
+const destinationInput = document.querySelector('.drop')
 // API apiCalls
 
 function fetchData() {
@@ -43,12 +41,30 @@ function assignData(index) {
     const fetchedDestinationData = promises[2].destinations
     traveler = new Traveler(fetchedTravelerData[index])
     travelerData = fetchedTravelerData.map(traveler => {
-      return new Traveler(traveler)
-    })
+      return new Traveler(traveler)})
     tripData = fetchedTripData.map(trip => {
       return new Trip(trip)
     })
     destinationData = fetchedDestinationData
     displayTravelerInfo(traveler, tripData, destinationData)
   }))
+}
+
+// <<<>>> EVENT HANDLERS <<<>>>
+
+function displayTravelerInfo(traveler, tripData, destinationData) {
+  traveler.findAllTrips(tripData, destinationData);
+  traveler.sortTrips();
+  traveler.calculateAmountSpentThisYear();
+  domUpdates.greetUser(traveler);
+  domUpdates.displayTripCards(traveler, destinationData, 'allTrips');
+  displayTripForm(destinationData);
+}
+
+function displayTrips(event) {
+  domUpdates.displayTripCards(traveler, destinationData, event.target.id)
+}
+
+function displayTripForm(destinationData) {
+  domUpdates.createDropMenu(destinationData);
 }
