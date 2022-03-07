b2fa38a9c31a973915f40b5491f2ca43dc0487da
Ali Nooner <alinooner@gmail.com>
2022-03-07 11:23:02 -0600

    Add postAPIData functionality

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,10 +4,16 @@ export const fetchAPIData = (type) => {
   .catch(error => console.log('error: ', error))
 }
 
-// const fetchAPIData = (type) => {
-//   return fetch(`http://localhost:3001/api/v1/${type}`)
-//   .then(response => response.json())
-//   .catch(error => console.log('error: ', error))
-// }
-//
-// export
+export const postAPIData = (newTrip) => {
+  return fetch ('http://localhost:3001/api/v1/',
+    {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(newTrip),
+    })
+    .then(response => response.json())
+    .then(json => console.log())
+    .catch(err => 'ERROR')
+}
